7e434e53c35111cd85023633c8463ac3b5bd84a2
Jonny Coe <jon@northinteractive.co>
2020-11-12 20:54:56 +0000

    working dataset

--- perion-v1.py
+++ perion-v1.py
@@ -9,7 +9,8 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 import pathlib
-dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_iamges/flower_photos.tgz"
+
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
 data_dir = pathlib.Path(data_dir)
 
