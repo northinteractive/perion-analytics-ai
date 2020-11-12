93d39f12ca597589e6e5e6cac6933bb546a2dec4
Jonny Coe <32171526+northinteractive@users.noreply.github.com>
2020-11-12 14:21:57 -0700

    Model trained

--- perion-v1.py
+++ perion-v1.py
@@ -10,6 +10,8 @@ from tensorflow.keras.models import Sequential
 
 import pathlib
 
+# Get the dataset from googleapi
+
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
 data_dir = pathlib.Path(data_dir)
@@ -47,8 +49,49 @@ class_names = train_ds.class_names
 for image_batch, labels_batch in train_ds:
     print(image_batch.shape)
     print(labels_batch.shape)
-    break
+    break1
 
+# configure the dataset for performance
+# .cache keeps the images in memory after they're loaded off disk during first epoch
+# .prefetch overlaps data preprocessing and model execution while training.
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+# Standarize - rescale [0,255] to [0,1] to standardize values for neural network
+normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+
+# Create the sequential model
+
+num_classes = 5
+
+model = Sequential([
+    layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
+])
+
+# Compile the model (using Adam Optimizer and losses.SparseCategoricalCrossentropy).
+# To view training and validation for each metric, pass metrics.
+
+model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),metrics=['accuracy'])
+
+# Get the model Summary
+
+model.summary()
+
+# Lets Train the model
+
+epochs=10
+history = model.fit(train_ds, validation_data=val_ds,epochs=epochs)
 
 # print(class_names)
 
