cb1c7e4c2feeee3481e9aecfa7800f8cbacfe775
Jonny Coe <32171526+northinteractive@users.noreply.github.com>
2020-11-12 14:06:43 -0700

    added visualization

---
+++ v1-visualize.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt 
+plt.figure(figsize=(10,10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3,3,i+1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
