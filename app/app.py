84da02035f8c579e2772af10f8493a0038cbd3d5
Jonny Coe <32171526+northinteractive@users.noreply.github.com>
2020-11-12 15:04:14 -0700

    added API

--- app/app.py
+++ app/app.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return """
-    <h1>Test Docker</h1>
-    <p>Works</p>
+    <h1>Welcome to Perion AI Engine</h1>
+    <p><a href='/api'>Access the API</a></p>
     """
+
+@app.route("/api", methods=['GET'])
+    def index():
+        if request.method == 'GET':
+            dataset_url = request.args.get('url', '')
+            return dataset_url
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
