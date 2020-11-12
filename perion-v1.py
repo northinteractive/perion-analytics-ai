6e2f5bb8742a9a12591fb5dffe8cf07ff38c009c
Jonny Coe <32171526+northinteractive@users.noreply.github.com>
2020-11-12 13:57:45 -0700

    Initial Keras Training _ds

--- perion-v1.py
+++ perion-v1.py
@@ -14,5 +14,18 @@ dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_im
 data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
 data_dir = pathlib.Path(data_dir)
 
-image_count = len(list(data_dir.glob('*/*.jpg')))
-print(image_count)
+# image_count = len(list(data_dir.glob('*/*.jpg')))
+# print(image_count)
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size
+)
