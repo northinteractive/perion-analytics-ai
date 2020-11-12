585369d5eba0453e5fd2269a5222bafc51771ee1
Jonny Coe <32171526+northinteractive@users.noreply.github.com>
2020-11-12 14:01:15 -0700

    added keras validation layer

--- perion-v1.py
+++ perion-v1.py
@@ -22,9 +22,18 @@ img_height = 180
 img_width = 180
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    data_dir,
+    data_dir, 
     validation_split=0.2,
     subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size
+)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
     seed=123,
     image_size=(img_height, img_width),
     batch_size=batch_size
