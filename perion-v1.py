bf74c060774c5505a406c2bf47f3c2a7166232ca
Jonny Coe <32171526+northinteractive@users.noreply.github.com>
2020-11-12 14:07:55 -0700

    Removed visualize.py and updated perion-v1.py

--- perion-v1.py
+++ perion-v1.py
@@ -41,3 +41,12 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
 
 class_names = train_ds.class_names
 print(class_names)
+
+import matplotlib.pyplot as plt 
+plt.figure(figsize=(10,10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3,3,i+1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
